a544e0d926b8be6c52e3ebeeb313d3963aa76ae9
Sergey Klunniy <s.klunniy@gmail.com>
2019-11-30 08:54:32 +0200

    assert -ea!

---
+++ src/main/java/mate/academy/lessons/lesson9G/Main.java
@@ -0,0 +1,10 @@
+package mate.academy.lessons.lesson9G;
+
+public class Main {
+    public static void main(String[] args) {
+        // -ea
+        int a = 3;
+        int b = 3;
+        assert a == b : "Wrong equals";
+    }
+}
