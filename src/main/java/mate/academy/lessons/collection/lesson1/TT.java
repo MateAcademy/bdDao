73343944872359692ddddc0808b9319c4f559e7a
Sergey Klunniy <s.klunniy@gmail.com>
2019-12-08 11:54:36 +0200

    начал изучать Коллекции

---
+++ src/main/java/mate/academy/lessons/collection/lesson1/TT.java
@@ -0,0 +1,13 @@
+package mate.academy.lessons.collection.lesson1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Sergey Klunniy
+ */
+public class TT {
+    public static void main(String[] args) {
+        List<Integer> list= new  ArrayList<>();
+    }
+}
