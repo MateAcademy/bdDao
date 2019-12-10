4dc437c08ccc3fb4d0b4e52d76bccd4b2657aaf4
Sergey Klunniy <s.klunniy@gmail.com>
2019-12-10 15:08:28 +0200

    продолжаю коллекции Коллекции

--- src/main/java/mate/academy/lessons/collection/stepik/Test1.java
+++ src/main/java/mate/academy/lessons/collection/stepik/Test1.java
@@ -1,5 +1,7 @@
 package mate.academy.lessons.collection.stepik;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,11 +11,26 @@ import java.util.List;
 public class Test1 {
     public static void main(String[] args) {
 
-    }
+            List<String> list = Arrays.asList("hi hello goodmorning ass".split("\\s"));
+            changeList(list).stream().forEach(System.out::println);
+        }
 
-    public static Integer maxElem(List<Integer> list) {
-        int max = Collections.max(list);
-        return max;
+
+
+        static List<String> changeList(List<String> list) {
+
+            List<String> list2 = new ArrayList<>();
+        String max = Collections.max(list, (o1, o2) -> {
+            if (o1.length()<o2.length()) return -1;
+            if (o1.length()>o2.length()) return 1;
+            return 0;
+        });
+
+            for (int i = 0; i < list.size(); i++) {
+                list2.add(max);
+            }
+            return list2;
+        }
+
 
     }
-}
